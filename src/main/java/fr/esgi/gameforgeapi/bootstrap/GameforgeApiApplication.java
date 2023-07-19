c9d3ce73e093a41ffbf50f5acc9f05233bbf2037
Den <delton.denice@gmail.com>
2023-07-19 22:23:37 +0200

    fix security

--- src/main/java/fr/esgi/gameforgeapi/bootstrap/GameforgeApiApplication.java
+++ src/main/java/fr/esgi/gameforgeapi/bootstrap/GameforgeApiApplication.java
@@ -3,27 +3,20 @@ package fr.esgi.gameforgeapi.bootstrap;
 import fr.esgi.gameforgeapi.bootstrap.config.GameforgeApiConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Import({GameforgeApiConfiguration.class})
-@SpringBootApplication(scanBasePackages = "fr.esgi")
+@SpringBootApplication(scanBasePackages = "fr.esgi", exclude = {SecurityAutoConfiguration.class })
 public class GameforgeApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GameforgeApiApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/gameforge-api").allowedOrigins("http://localhost:4200");
-            }
-        };
-    }
+
 
 }
