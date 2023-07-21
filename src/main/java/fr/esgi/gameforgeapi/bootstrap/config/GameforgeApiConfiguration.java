d7a217a7b951538c3b8201992709a02bd0bf307b
Barlords <basile.pulin@gmail.com>
2023-07-21 02:12:56 +0200

    fix CORS

--- src/main/java/fr/esgi/gameforgeapi/bootstrap/config/GameforgeApiConfiguration.java
+++ src/main/java/fr/esgi/gameforgeapi/bootstrap/config/GameforgeApiConfiguration.java
@@ -7,10 +7,19 @@ import io.vavr.jackson.datatype.VavrModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 @Import(DomainConfiguration.class)
-public class GameforgeApiConfiguration {
+public class GameforgeApiConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
