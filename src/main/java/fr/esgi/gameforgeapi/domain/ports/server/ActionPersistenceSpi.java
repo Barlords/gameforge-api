bc9dc2a7dc5b77c4c4f2b43a201409b9dba91912
lucas <lucas.953501@gmail.com>
2023-07-24 23:08:19 +0200

    add create get action

--- src/main/java/fr/esgi/gameforgeapi/domain/ports/server/ActionPersistenceSpi.java
+++ src/main/java/fr/esgi/gameforgeapi/domain/ports/server/ActionPersistenceSpi.java
@@ -1,13 +1,11 @@
 package fr.esgi.gameforgeapi.domain.ports.server;
 
 import fr.esgi.gameforgeapi.domain.functional.models.Action;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ActionPersistenceSpi extends PersistenceSpi<Action, UUID> {
 
-    @Transactional
     List<Action> findByLobbyId(UUID lobbyId);
 }
