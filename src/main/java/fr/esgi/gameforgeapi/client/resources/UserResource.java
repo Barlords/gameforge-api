40cbc439b2d09841d938a1a3437226f3ed5861a2
Barlords <basile.pulin@gmail.com>
2023-07-25 10:16:35 +0200

    patch user

--- src/main/java/fr/esgi/gameforgeapi/client/resources/UserResource.java
+++ src/main/java/fr/esgi/gameforgeapi/client/resources/UserResource.java
@@ -43,7 +43,7 @@ public class UserResource {
                 .toList();
     }
 
-    @GetMapping("/getById/{id}")
+    @GetMapping("/id/{id}")
     @ResponseStatus(OK)
     public UserDto getUserById(@PathVariable String id) {
         return userFinderApi.findById(UuidValidator.validate(id))
@@ -51,7 +51,7 @@ public class UserResource {
                 .orElseThrow(() -> new ResourceNotFoundException("L'utilisateur " + id + " est introuvable"));
     }
 
-    @GetMapping("/getByToken/{token}")
+    @GetMapping("/token/{token}")
     @ResponseStatus(OK)
     public UserDto getUserByToken(@PathVariable String token) {
         return userFinderApi.findByToken(UuidValidator.validate(token))
@@ -59,7 +59,7 @@ public class UserResource {
                 .orElseThrow(() -> new ResourceNotFoundException("L'utilisateur avec le token" + token + " est introuvable"));
     }
 
-    @GetMapping("/getByEmail/{email}")
+    @GetMapping("/email/{email}")
     @ResponseStatus(OK)
     public UserDto getUserByEmail(@PathVariable String email) {
         return userFinderApi.findByEmail(email)
