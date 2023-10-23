baf4cd52efb4629de470d5909fe7a4f483068475
Barlords <basile.pulin@gmail.com>
2023-10-23 16:49:29 +0200

    patch friend request

--- src/main/java/fr/esgi/gameforgeapi/bootstrap/GameforgeApiApplication.java
+++ src/main/java/fr/esgi/gameforgeapi/bootstrap/GameforgeApiApplication.java
@@ -14,6 +14,4 @@ public class GameforgeApiApplication {
         SpringApplication.run(GameforgeApiApplication.class, args);
     }
 
-
-
 }
--- src/main/java/fr/esgi/gameforgeapi/bootstrap/config/client/ClientConfiguration.java
+++ src/main/java/fr/esgi/gameforgeapi/bootstrap/config/client/ClientConfiguration.java
@@ -1,14 +1,11 @@
 package fr.esgi.gameforgeapi.bootstrap.config.client;
 
-import fr.esgi.gameforgeapi.client.services.EmailSenderService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 
 @Configuration
 @ComponentScan("fr.esgi")
 public class ClientConfiguration {
+
 
 }
